8c03b9bb693b0986aba91b489f22c3b411747284
liberbell <liberbell@gmail.com>
2018-10-10 08:46:09 +0900

    url1

--- form/urls.py
+++ form/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^myapp1/', include('myapp1.urls', namespace='myapp1')),
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
 ]
